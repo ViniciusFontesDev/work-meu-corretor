9fca2b27f8c9c18b84decdb0916b2790779799ad
Vinícius Fontes <programadorviniciusfontes@gmail.com>
2024-12-16 17:59:39 -0400

    Quase pronto...
    Falta fazer ligações entre links, caso queira falta colocar os imóveis corretamente, cada um quer for anunciar. Além disso posso ainda fazer algumas modificações com javascript.

---
+++ script.js
@@ -0,0 +1,16 @@
+/*
+const imagens = document.querySelectorAll("#galeria ul li img");
+
+function galeriaTrocar(event){
+    const principal = document.querySelector("#galeria #imagem-principal");
+    const clicada = event.currenttarget;
+    principal.src = clicada.src;
+    principal.alt = clicada.alt;
+}
+
+function galeriaClique(imagem){
+    imagem.addEventListener("click", galeriaTrocar);
+}
+
+imagens.forEach(galeriaClique);
+*/
